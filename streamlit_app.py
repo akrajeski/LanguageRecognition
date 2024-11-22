4d9fba7c04dd2a30718ecbdd78c41352c8de188b
Anna <55714854+akrajeski@users.noreply.github.com>
2024-11-22 21:48:56 +0000

    Training data confirmed! Added to /TrainingData -> files with appropriate sources Created file for parsed training data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("German ")
+st.title("So how's your fluency? 😏 ")
 st.write(
-    "Let's start buildGing! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "This application is for those who wish to test their fluency level with German"
+    )
